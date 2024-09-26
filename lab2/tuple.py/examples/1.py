15f66cd7ec94950b0346eeba5d1a709594aeb595
AlinaSabirovaa <a_sabirova@kbtu.kz>
2024-09-26 12:49:13 +0500

    Add files via upload

---
+++ lab2/tuple.py/examples/1.py
@@ -0,0 +1,6 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+#Tuple is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Set, and Dictionary, all with different qualities and usage.
+#A tuple is a collection which is ordered and unchangeable.
+
